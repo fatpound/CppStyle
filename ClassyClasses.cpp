c09b9c9ebaad552433db15e593b67c98c7c0e915
fatpound <84704675+fatpound@users.noreply.github.com>
2025-02-09 17:53:44 +0300

    Update ClassyClasses.cpp

--- ClassyClasses.cpp
+++ ClassyClasses.cpp
@@ -38,15 +38,14 @@ module; // I add that to the top of module files
 
 // unix C libraries if needed e.g. <unistd.h>
 
-#include <stdio.h>       // c libraries
+#include <stdio.h>       // standard c libraries
 #include <stdlib.h>
 
-#include <cmath>         // c libraries for c++
-#include <cstdlib>       // c libraries for c++
+#include <cmath>         // standard c libraries for c++
+#include <cstdlib>       // standard c libraries for c++
 
-#include <string>        // c++ libraries last
-// latest c++ libs may and should appear at the bottom, like c++23's stacktrace
-// but thats not important actually
+#include <string>        // standard c++ libraries
+// latest std c++ libs should appear at the bottom, like c++23's stacktrace
 
 // defines -> between includes and pragmas
 #define FATPOUND
@@ -64,7 +63,9 @@ module; // I add that to the top of module files
 
 // (export) module FatPound; // this is obviously here
 
-// I prefer using namespace aliases in the cpp file, if we're not in an ixx file
+// I prefer using namespace aliases in global namespace scope ONLY in the regular cpp file
+// if we're in a module unit (file), it does not matter that much. But even if I use modules,
+// I use namespace aliases in module implementation units (files).
 namespace fat = fatpound;
 namespace snd = fatpound::sandbox
 
@@ -76,11 +77,13 @@ namespace fs = std::filesystem;
 namespace rn = std::ranges;
 namespace vi = std::views;
 
-// I dont like "using namespace" in the global scope in a file;
-// If I were to use it, I would only use it in small scopes, function scopes or cpp file scope
-// like here, before 'everything' such as namespace or class declarations or functions
+// I dont like "using namespace" in the global scope in a file, BUT
+// If I were to use it, I would only use it in small scopes, small C++ test codes,
+// function scopes
+// If I just test some C++ standard compliance test of a compiler, I would use it anywhere like here, before 'everything'
 
 // BUT, I may use using namespace std::literals; here... idk
+// They should obey the rule as using namespace ...'s rules;
 
 // THE CLASS
 // friends first
@@ -93,7 +96,7 @@ namespace vi = std::views;
 // ^^^ even if there's nothing in the class, I still write
 // all of these access specifiers (only once in this context)
 
-// I dont start class names with a 'C' but I may apply this style in the future
+// I dont start class names with a 'C' but I might apply this style in the future
 class CClass
 {
 public:
@@ -114,17 +117,18 @@ private:
 // class
 // union
 
-// abstract structures appear first, for example:
+// abstract (interface) structures appear first, for example:
 //
 // public:
-// struct SAbstract {}; // this is abstract
-// struct SNormal {};   // this is normal
+// struct SITest {};
+// struct SAbstract {};
+// struct SNormal {};
 
 // and then.............
 // operators
-// static functions
-// virtual methods
-// methods
+// static member functions     // function templates appear first in every category
+// virtual member functions
+// member functions
 
 // this rule is applied to method ordering
 // method()
@@ -138,30 +142,56 @@ namespace test
 
 template <typename T>
 requires Concept<T> // I may put that after the template, not on a newline but idk
-class CTest final : virtual public Base
+class CTest final : virtual public Base // first virtual, then access specifier keyword
 {
     friend class FC;
     friend ::test::FCC; // I dont use this declaration style (no class keyword)
     //     ^^ I probably will use the scope resolution operator if the class is in another namespace
     // in this case, I shouldn't add it since CTest class is just in the global namespace
 
+    // static constexpr member variables are always on top, unless they are non-private
+    // their names start with "scx_"
+    // static constinit member variables are almost the same, but their names start with "sci_"
+    // AND see member variable naming rules, at the bottom of this class
+    // AND constinit member variables appear first
+    static constinit int sci_special_val_;
+
+    static constexpr int scx_int2 = 100;
+    static constexpr auto scx_text_ = L"FatPound";
+
+    typedef auto(*pf0)() -> void;
+    typedef void(*pf1)();
+
+    typedef int arr1d_t[7];
+    typedef int arr2d_t[7][7];
+    typedef int arr3d_t[7][7][7];
+
+    typedef std::uint8_t BYTE;
+
+    using Vector_t = std::vector<T>; // type aliases, following friend class declarations
+
     using Base::Base;
 
-    using Base::StaticMethod1;      // I don't recommend this
-    using Base::StaticMethod2_;
+    using Base::StaticMemberFunc1;   // I don't recommend this
+    using Base::StaticMemberFunc2_;
 
-    using Base::member1;            // also, I don't like this, I may use Base members by writing this->
+    using Base::member1;             // also, I don't like this, I may use Base members by writing Base:: or maybe this->
     using Base::member2_;
-        // <- only one space here
+    // <- only one space here
+public:
+    using type       = CTest; // non private type aliases
+    using value_type = T;
+
+
 public:
     enum E
     {
 
     };
-        // <- two empty lines between every access specifier
-        // except for nested structures
+    // <- two empty lines between every access specifier
+    // except for nested structures, they should have only 1 space between those access specifiers.
 public:
-    enum struct ES // I can add final on scoped enums only on MSVC but IntelliSense gets angry
+    enum struct ES // I can add final on scoped enums only on MSVC but IntelliSense complains
     {
 
     };
@@ -179,6 +209,8 @@ public:
     {
 
     };
+    struct Strak  {}; // if there is a need for empty base classes, like, for template mp, I can one-line a structure
+    struct Strakt {};
 
 
 public:
@@ -219,18 +251,109 @@ public:
 
 
 public:
+    // I like adding a space between operator keyword and the operator token
+    auto operator ++ ()    -> CTest&;
+    auto operator ++ (int) -> CTest;
+    auto operator -- ()    -> CTest&;
+    auto operator -- (int) -> CTest;
+
+    auto operator +  () -> CTest;
+    auto operator -  () -> CTest;
+    
+    auto operator +  (const CTest& rhs) -> CTest;
+    auto operator -  (const CTest& rhs) -> CTest;
+    auto operator *  (const CTest& rhs) -> CTest;
+    auto operator /  (const CTest& rhs) -> CTest;
+    auto operator %  (const CTest& rhs) -> CTest;
+    auto operator += (const CTest& rhs) -> CTest&;
+    auto operator -= (const CTest& rhs) -> CTest&;
+    auto operator *= (const CTest& rhs) -> CTest&;
+    auto operator /= (const CTest& rhs) -> CTest&;
+    auto operator %= (const CTest& rhs) -> CTest&;
+
+    auto operator ^  (const CTest& rhs) -> auto;
+    auto operator |  (const CTest& rhs) -> auto;
+    auto operator &  (const CTest& rhs) -> auto;
+    auto operator ^= (const CTest& rhs) -> auto;
+    auto operator |= (const CTest& rhs) -> auto;
+    auto operator &= (const CTest& rhs) -> auto;
+
+    // auto operator xor    (const CTest& rhs) -> auto;
+    // auto operator bitor  (const CTest& rhs) -> auto;
+    // auto operator bitand (const CTest& rhs) -> auto;
+    // auto operator xor_eq (const CTest& rhs) -> auto;
+    // auto operator or_eq  (const CTest& rhs) -> auto;
+    // auto operator and_eq (const CTest& rhs) -> auto;
+
+    auto operator <=> (const CTest& rhs) -> auto;
+
+    auto operator ==  (const CTest& rhs) -> bool;
+    auto operator !=  (const CTest& rhs) -> bool; // not_eq
+    auto operator >   (const CTest& rhs) -> bool;
+    auto operator <   (const CTest& rhs) -> bool;
+    auto operator >=  (const CTest& rhs) -> bool;
+    auto operator <=  (const CTest& rhs) -> bool;
+
+    auto operator !  () -> bool;   // not
+    auto operator &  () -> CTest*;
+    auto operator -> () -> CTest*;
+    auto operator ~  () -> CTest;  // compl
+    auto operator ,  (const T& val) -> CTest*;
+    auto operator [] (const std::size_t& index) -> T;
     auto operator () () -> std::string;
-    // I like putting a space between parantheses and operator keyword
+    auto operator co_await () -> auto;
+
+    explicit(true)  operator int ();
+    explicit        operator double ();
+    explicit(false) operator char ();
+
+    template <typename T>
+    operator T ();
+
+    operator void ();
 
 
 public:
-    // I need to re-think this order but it seems OK
-
+    // concept + requires + requires templated methods
     // concept + requires templated methods
     // concept templated methods
     // requires templated methods
     // templated methods
-    // TO BE CONTINUED
+
+    // abbreviated template syntax:
+    // constrained auto first
+    // auto last
+
+    // template <std::integral N>
+    // requires requires { requires true; }
+    // static void s() requires requires { requires true; };
+
+    template <std::integral N>
+    requires true
+    static void s() requires true;
+
+    template <std::integral N>
+    requires true
+    static void s();
+    
+    template <std::integral N>
+    static void s() requires true;
+
+    template <std::integral N>
+    static void s();
+
+    template <typename T>
+    static void s() requires true;
+
+    template <typename T>
+    static void s();
+
+    static void s(std::integral auto N);
+    static void s(auto N);
+
+    // non templated member functions are next
+    // noexcept specifier order is like this
+    static void s1() noexcept(noexcept(true));
 
     static void s1() noexcept(IN_RELEASE);
 
@@ -244,14 +367,14 @@ public:
 
 
 public:
-    // methods are ordered by return types FIRST
+    // virtual methods must be specified with virtual keyword
     // then override specifiers in the following order:
 
-    virtual void v1() final = 0;       // pure virtual final (if needed such a thing)
+    virtual void v1() final = 0;       // pure virtual final ? (if needed such a thing)
 
     virtual void v2() = 0;             // pure virtual
 
-    virtual void v3() override final;  // virtual override final
+    virtual void v3() override final;  // virtual override final (I do not like "final override")
 
     virtual void v4() final;           // virtual final
 
@@ -261,31 +384,70 @@ public:
 
 
 public:
-    // primitive types with more bytes first in every category
-    // floating points first, then integrals
+    // all methods are ordered by return types FIRST
+    // primitive types with more storage space appear first in every category
+    // real numbers first, then integrals
 
-    // I will explain auto's here later on
+    auto f0() -> std::integral auto;
+    
+    auto f0() -> auto;
+
+    auto f0() -> CTest;
+    auto f0() -> CTest*;
+    auto f0() -> CTest&;
 
     long double f1();
+
     double f2();
 
     float f3();
 
+    signed long long int f4();
+    signed long long f4();
     long long int f4();
-    long int f5();
-    int f6(); 
-    short int f7();             
-    unsigned int f8();      // I don't like writing unsigned (or signed) without the type because it may be char too
+    long long f4();
 
-    signed char f9();       // If signed is written explicitly, it comes before regular one
+    unsigned long long int f4();
+    unsigned long long f4();
+
+    signed long int f4();
+    signed long f4();
+    long int f4();
+    long f4(); 
+
+    signed int f();
+    signed f();
+    int f6();
+
+    unsigned int f8();
+    unsigned f();
+
+    signed short int f7();
+    signed short f7();
+    short int f7();
+    short f7();
+
+    unsigned short int f7();
+    unsigned short f7();
+
     char f10();
+    signed char f9();
     unsigned char f11();
 
     bool f12();
 
-    void f13();
-    void f14() &;
+    // If cvr qualifiers are written in method declarations / definitions, then it must be in this order:
+    void f13() &;
+    void f13() &&;
+    void f14();
+    void f15() &;
+    void f15() const &;
+    void f15() volatile &;
+    void f15() const volatile &;
     void f15() &&;
+    void f15() const &&;
+    void f15() volatile &&;
+    void f15() const volatile &&;
 
 
 protected:
@@ -294,29 +456,31 @@ protected:
 
 
 private:
-    // static variables' names start with an "s_"
-    // member variables' names start with an "m_"
-    // pointers, references, and "handle"s are written in camelCase, not snake_case like other member variables.
-    ::Microsoft::WRL::ComPtr<ID3D11Device> m_pDevice_ = nullptr;
-    ::HWND m_hWnd_ = nullptr;
+    // member variable naming rules
+    // static   ones start with an "s*_"
+    // member   ones start with an "m*_"
+    // const    ones start with an "*c_"
+    // volatile ones start with an "*v_"
+
+    // inlines first
+    inline static bool s_flag_ = true;
+
+    static int s_ival_;
+    static const int sc_ival_;
+
+
+private:
+    // pointers, references, and "handle"s are written in camelCase,
+    // not snake_case like other member variables.
+    Microsoft::WRL::ComPtr<ID3D11Device> m_pDevice_ = nullptr; // I might change this naming rule for smart pointer objects, in the future.
+    HWND m_hWnd_ = nullptr;
 
     std::vector<int> m_vec_;
 
     std::string m_str_;
 
-    T m_var;
-
-    inline static bool s_flag = true;
-
-    static int s_integer;
-
-    static constinit int s_int1;          
-
-    static constexpr int s_int2;
-    
-    static constexpr auto s_text_ = L"FatPound";
+    T m_var_;
 };
 
 module : private;
 // I don't add an empty newline at the end if I use MSVC
-// to be continued
